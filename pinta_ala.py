89c3bcb3ff94b7b8bb0e7fcdfc2260b00bea1ea0
naima <naimaibr@metropolia.fi>
2023-08-23 11:38:59 +0300

    this is our project initial state.

---
+++ pinta_ala.py
@@ -0,0 +1,14 @@
+print("moikka")
+
+# nimi = "naima"
+nimi = input("mikä on sinun nimesi: ")
+
+print("hei, sinun nimi on" + nimi)
+
+input("anna ympyrän säde: ")
+
+import math
+radius = float (input("please give me the radius of the circle to calculate the area: "))
+area = math.pi * (radius * radius)
+print(f"the area is: {area}")
+print(f"the area is: {area:5.2f}")

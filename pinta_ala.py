0cfb6610de0847178e934c6b29071cf5ce040e53
naima <naimaibr@metropolia.fi>
2023-08-26 13:01:05 +0300

    The fourth question let gooo!

--- pinta_ala.py
+++ pinta_ala.py
@@ -1,32 +1,13 @@
-print("moikka")
 
+# lasketaan kolme luku arvojen keskenaan
 
-# nimi = "naima"
-nimi = input("mikä on sinun nimesi: ")
+A = float(input("Anna esimmäinen luku: "))
+B = float(input("Anna toinen luku: "))
+C = float(input("Anna kolmes luku: "))
 
-print("hei, sinun nimi on" + nimi)
-
-input("anna ympyrän säde: ")
-
-import math
-radius = float (input("please give me the radius of the circle to calculate the area: "))
-area = math.pi * (radius * radius)
-print(f"the area is: {area}")
-print(f"the area is: {area:5.2f}")
-
-
-
-import math
-#lasketaan suorakolmoinen kannan ja korkeus
-
-A = float(input("Anna 1. kateetin pituus:"))
-B = float(input("Anna 2. kateetin pituus:"))
-
-# laskee hypotenuus
-C = math.sqrt(A**2 + B**2)
-print(C)
-
-print("Hypotenuus pituus on",C)
+# Lasketaan keksiarvo
+D = (A+B+C)/3
+print("lukujen keskiarvo on", D)
 
 
 

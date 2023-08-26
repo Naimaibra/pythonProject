ebb7d73cfc83df085f2e57f6655c81bfb0d7a109
naima <naimaibr@metropolia.fi>
2023-08-26 12:01:00 +0300

    tämä on kolmas kysymys lakse suorakolmoinen kanna/korkeus

--- pinta_ala.py
+++ pinta_ala.py
@@ -12,3 +12,16 @@ radius = float (input("please give me the radius of the circle to calculate the 
 area = math.pi * (radius * radius)
 print(f"the area is: {area}")
 print(f"the area is: {area:5.2f}")
+
+
+import math
+#lasketaan suorakolmoinen kannan ja korkeus
+
+A = float(input("Anna 1. kateetin pituus:"))
+B = float(input("Anna 2. kateetin pituus:"))
+
+# laskee hypotenuus
+C = math.sqrt(A**2 + B**2)
+print(C)
+
+

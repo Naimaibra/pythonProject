f9e07159edac7ada5e18582ba2d490cdb9ac1fb1
naima <naimaibr@metropolia.fi>
2023-08-28 10:42:23 +0300

    Moduuli03

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("hello naima")
+import math
+#lasketaan suorakolmoinen kannan ja korkeus
+
+A = float(input("Anna 1. kateetin pituus:"))
+B = float(input("Anna 2. kateetin pituus:"))
+
+# laskee hypotenuus
+C = math.sqrt(A**2 + B**2)
+print(C)
+
+
+
+
+

7b9156ad7bcca60ab881549471b8eefcefcbdc34
naima <naimaibr@metropolia.fi>
2023-10-24 04:14:16 +0300

    moduuli09, 1-4 tehtävät

--- Moduuli09/tehtävä04.py
+++ Moduuli09/tehtävä04.py
@@ -1,45 +1,49 @@
+
+from colorama import init, Fore
 import random
 
+# Initialize colorama
+init(autoreset=True)
+
 class Car:
-    def __init__(self, registration_number, max_speed):
-        self.registration_number = registration_number
+    def __init__(self, register_number, max_speed):
+        self.register_number = register_number
+        self.odometer = 0
+        self.speed = 0
         self.max_speed = max_speed
-        self.current_speed = 0
-        self.travelled_distance = 0
-
-    def accelerate(self, change):
-        if change > 0:
-            self.current_speed += change
-        else:
-            self.current_speed = max(0, self.current_speed + change)
-        self.current_speed = min(self.current_speed, self.max_speed)
-
-    def drive(self, hours):
-        distance = self.current_speed * hours
-        self.travelled_distance += distance
-
-def main():
-    car_list = []
-
-    for i in range(1, 11):
-        registration_number = f"ABC-{i}"
-        max_speed = random.randint(100, 200)
-        car = Car(registration_number, max_speed)
-        car_list.append(car)
-
-    race_distance = 0
-
-    while race_distance < 10000:
-        for car in car_list:
-            change_in_speed = random.randint(-10, 15)
-            car.accelerate(change_in_speed)
-            car.drive(1)
-            race_distance = max(race_distance, car.travelled_distance)
 
 
-    print(f"{'Registration Number':<15}{'Max Speed (km/h)':<20}{'Current Speed (km/h)':<25}{'Travelled Distance (km)':<30}")
-    for car in car_list:
-        print(f"{car.registration_number:<15}{car.max_speed:<20}{car.current_speed:<25}{car.travelled_distance:<30}")
+    def print_info(self):
+        print(f"Bugatti{Fore.YELLOW}" f"Huippunopeus{self.max_speed}" f"matkamittari{self.odometer}" f"nopeus {self.speed}")
+
+    def accelerate(self, speed_change):
+        if 0 < self.speed + speed_change < self.max_speed:
+            self.speed = self.speed + speed_change
+
+    def walk(self, hours):
+        if self.odometer > -1:
+            self.odometer = self.speed * hours + self.odometer
+
+cars = []
+for i in range(10):
+    car = Car("ABC-" + str(i + 1), random.randint(100, 200))
+    cars.append(car)
+
+race_over = False
+
+while not race_over:
+    for car in cars:
+        car.walk(1)
+        car.accelerate(random.randint(-10, 15))
+
+    for car in cars:
+        if car.odometer >= 10000:
+            race_over = True
+
+for car in cars:
+    car.print_info()
 
 
-
+for car in cars:
+    if car.odometer >= 10000:
+        print(f"{Fore.BLUE}Voittaja on {car.register_number}")
